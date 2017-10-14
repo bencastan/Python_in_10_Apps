66fdd7e6f200f826bcb5d984a4dda6bef4e22ef5
Ben Castan <ben@castanfamily.com>
2017-10-14 14:22:10 +1100

    Committed to consolidated Repo

--- 01_Hello_world/program.py
+++ 01_Hello_world/program.py
@@ -1,1 +1,8 @@
+print('-----------------------------------')
+print('          HELLO APP')
+print('-----------------------------------')
 
+name = input('What is your name? ')
+greeting = 'Nice to meet you ' + name
+
+print(greeting)
