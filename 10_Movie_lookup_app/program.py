25ac8f2db6236bde5e44d44859b472098d807003
Ben Castan <ben@castanfamily.com>
2017-11-23 14:11:50 +1100

    Fixed name for projetc 03

--- 10_Movie_lookup_app/program.py
+++ 10_Movie_lookup_app/program.py
@@ -1,3 +1,4 @@
+import requests
 import movie_svc
 
 
@@ -14,17 +15,24 @@ def print_header():
 
 
 def search_event_loop():
-    search = 'ONCE_THROUG_LOOP'
+    search = 'ONCE_THROUGH_LOOP'
     while search != 'x':
-        search = input("Movie search text (x to exit): ")
-        if search != 'x':
-            results = movie_svc.find_movies(search)
-            print("Found {} results".format(len(results)))
-            for r in results:
-                print("{} -- {}".format(
-                    r.year, r.title
-                ))
-            print()
+        try:
+            search = input("Movie search text (x to exit): ")
+            if search != 'x':
+                results = movie_svc.find_movies(search)
+                print("Found {} results".format(len(results)))
+                for r in results:
+                    print("{} -- {}".format(
+                        r.year, r.title
+                    ))
+                print()
+        except ValueError:
+            print("Search text is required")
+        except requests.exceptions.ConnectionError:
+            print("Error the network is down!...")
+        except Exception as x:
+            print("Unexpected error. Details: {}".format(x))
 
     print("Exiting...")
 
